113c7aefeab43f64b0dc93baa5af1123bd01a83c
quinlanalexander <quinlan.alexander@gmail.com>
2017-08-20 16:28:58 -0700

    [QA] clean up stuff

--- Documents/UCSC/CMPS12B/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------
-// HelloWorld.java
-// Prints "Hello world", the first program you should always write
-//-----------------------------------------------------
-class HelloWorld {
-    static public void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
